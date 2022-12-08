1cbfae5555f8a4e8f4a059e3082756b3469e9868
dop <fany.fop@gmail.com>
2022-12-07 21:25:52 -0600

    add validation when user is logged

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import { AuthService } from "../services/auth-service";
 
 export const ProtectedRoute = ({ children }) => {
-  if (AuthService.isAuthenticated) {
+  if (!AuthService.isAuthenticated()) {
     // user is not authenticated
     return <Navigate to="/" />;
   }
